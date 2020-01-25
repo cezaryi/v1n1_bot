5636c146dff24a23a3ce72db87fa533986cdc725
cezaryi <cezariomourao@hotmail.com>
2020-01-25 19:50:04 -0300

    start of the bot

--- telegram.js
+++ telegram.js
@@ -4,7 +4,28 @@ const TOKEN = `916708017:AAGMXnn9TS61RoD2E6Zr1XUDdtW3G9Lke_M`
 
 const bot = new TelegramBot( TOKEN, { polling: true } )
 
-bot.on('new_chat_members', (msg) => {
-    bot.sendMessage(msg.chat.id, `Olá ${msg.from.first_name}, bem vindo ao Devs SC!! Conte-nos um pouco sobre você, com o que trabalha e onde, se possivel é claro`)
- }) 
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    console.log(msg)
+
+    switch(msg.text.toLowerCase().trim()){
+        case '/start':
+            bot.sendMessage(chatId, 'Olá sou o bot v1n1, responsável por analisar os dados irregulares, você gostaria de receber notificações a respeito? Sim ou Não?');
+            break
+        
+        case 'sim':
+            bot.sendMessage(chatId, 'Você foi inscrito, eviaremos notificações assim que encontramos.');
+            break
+
+        case 'nao':
+        case 'não':
+            bot.sendMessage(chatId, 'Obrigado por me visitar, volte sempre! :D');
+            break
+
+        default:
+            bot.sendMessage(chatId, 'Você poderia escrever "/start" para que eu possa começar os trabalhos?');
+            break
+        }
+    
+  });
 
