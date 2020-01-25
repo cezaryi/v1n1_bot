8f406ecc8b6d239c3d452b2889f83c4b79dcbecd
cezaryi <cezariomourao@hotmail.com>
2020-01-25 02:33:00 -0300

    bot try modification

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const TOKEN = `916708017:AAGMXnn9TS61RoD2E6Zr1XUDdtW3G9Lke_M`
 
 const bot = new TelegramBot( TOKEN, { polling: true } )
 
-bot.on('new_chat_members', (msg) => {
-    bot.sendMessage(msg.chat.id, `Olá ${msg.from.first_name}, bem vindo ao Devs SC!! Conte-nos um pouco sobre você, com o que trabalha e onde, se possivel é claro`)
- })
+bot.on('message', (msg) => {
+    
+    var Hi = "hi";
+    if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
+    bot.sendMessage(msg.chat.id,"Hello dear user");
+    } 
+        
+    })
