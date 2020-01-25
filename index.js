7d57d04f7fb5d4b04c03e252f16755b7f3b6f2ee
cezaryi <cezariomourao@hotmail.com>
2020-01-25 15:31:07 -0300

    web crawler licitacoes

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
-const http = require("http");
+const R = require('ramda')
 
-const express = require("express");
+const tce_api = "https://api.tce.ce.gov.br/index.php/sim/1_0"
+const chowdown = require('chowdown');
+ 
+// Returns a promise
+chowdown('http://somewebpage.com')
+  .collection('.author', {
+    name: '.name',
+    age: '.age'
+  });
 
-const app = express();
+const getAllItensLicitacoes = async () => {
+    const chowdown_promises = []
+    for (var mun = 2; mun <= 185; mun ++) {
+        const munCode =  zeroFilled = ('000' + mun).substr(-3)
+        chowdown_promises.push(chowdown(`${tce_api}/itens_licitacoes?codigo_municipio=${munCode}&data_realizacao_licitacao=20191201_20200101`)
+            .collection('.itens_licitacoes', {
+                mun: '.codigo_municipio',
+                document: '.numero_documento_negociante',
+                item: '.descricao_item_licitacao',
+                valor_total: '.valor_vencedor_item_licitacao',
+                valor_unit: '.valor_unitario_item_licitacao',
+                licitacao: '.numero_licitacao' 
+            })
+            .catch(() => [])
+        )    
+    }
+    const lics = await Promise.all(chowdown_promises)
+    let itens_licitacoes = []
+    lics.map((lic) => {
+        itens_licitacoes = [...itens_licitacoes, ...lic]
+    })
+    return itens_licitacoes
+}
 
-app.post(
-
-http.createServer(express).listen(3000, () => console.log("Servidor rodando local na porta 3000"));
+getAllItensLicitacoes()
+.then((result) => console.log(result))
