3608627451fcdda572cf958c70962dcd1e676873
cezaryi <cezariomourao@hotmail.com>
2020-01-25 02:40:54 -0300

    returning to original

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const TOKEN = `916708017:AAGMXnn9TS61RoD2E6Zr1XUDdtW3G9Lke_M`
 const bot = new TelegramBot( TOKEN, { polling: true } )
 
 bot.on('new_chat_members', (msg) => {
-    bot.sendMessage(msg.chat.id, `Olá ${msg.from.second_name}, bem vindo ao Devs SC!! Conte-nos um pouco sobre você, com o que trabalha e onde, se possivel é claro`)
+    bot.sendMessage(msg.chat.id, `Olá ${msg.from.first_name}, bem vindo ao Devs SC!! Conte-nos um pouco sobre você, com o que trabalha e onde, se possivel é claro`)
  })
